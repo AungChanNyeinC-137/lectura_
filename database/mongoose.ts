289ce600ebe0e4deacd70538883543f659ea8c1d
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2026-03-01 16:07:55 +0630

    feat: mongodb connection
    created mongodb connection with cache promise for better API calls

---
+++ database/mongoose.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const MONGODB_URI = process.env.MONGODB_URI;
+if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+}
+
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null,
+        promise: Promise<typeof mongoose> | null
+    };
+}
+let cached = global.mongooseCache || (global.mongooseCache = { conn: null, promise: null });
+
+export const connectToDatabase = async () => {
+    if (cached.conn) return cached.conn;
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {
+            bufferCommands: false
+        });
+        try {
+            cached.conn = await cached.promise;
+
+        } catch (error) {
+            cached.promise = null;
+            console.error('Mongodb connection error.Please make sure mongodb is running.'+ error);
+            throw error;
+        }
+        console.info('Successfully connected to mongodb');
+        return cached.conn;
+
+
+    }
+}
